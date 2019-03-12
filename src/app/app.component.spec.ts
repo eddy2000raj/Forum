198cf600e61a2013f46c868375b1771d2fee68ba
rajesh <rajesh.kumar16@sap.com>
2019-03-12 10:01:16 +0530

    adding test case and css less

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,31 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Forum');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render the text search input box', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Forum!');
+    expect(compiled.querySelector('.search')).toBeDefined();
+  });
+
+  it('should render question in a label tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.label').textContent).toContain("What's Happening ?");
+  });
+
+  it('should render the write something input box', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.textWrite')).toBeDefined();
+  });
+
+  it('should render the post button', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.postButton')).toBeDefined();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, ElementRef ,OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Forum';
+  posts=[];
+  postData: string;
+
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('search') search: ElementRef;
+
+  ngOnInit() {
+  	if(localStorage.getItem("posts"))
+  	this.posts=JSON.parse(localStorage.getItem("posts"));
+  }
+
+  serachText(){
+
+   let text=this.search.nativeElement.value ;
+   let searchedPost={};
+   
+   if(text.length>0){
+
+     for(let i=0;i<this.posts.length;i++){
+      let post=this.posts[i]['postData'] ;
+      
+      if(post.indexOf(text)>-1){
+        searchedPost=this.posts[i];
+        }
+     }
+
+     this.posts=[];
+     this.posts[0]=searchedPost;
+
+   }else{
+    if(localStorage.getItem("posts"))
+    this.posts=JSON.parse(localStorage.getItem("posts"));
+   }
+   
+
+  }
+
+
+
+  addPost(){
+    //alert('clicked');
+    console.log('player name: ', this.nameInput.nativeElement.value);
+    let newPost={
+    	postData:this.nameInput.nativeElement.value,
+    	email:"john@Due",
+    	timestamp:new Date().getMilliseconds(),
+    	like:3,
+    	comments:[]
+    };
+
+
+   this.posts.push(newPost);
+   localStorage.setItem('posts',JSON.stringify(this.posts));
+   this.nameInput.nativeElement.value="";
+
+  }
 }
